9541bcdcb6830ec08df2858689190e88d27317d0
TejasKamat <kamattejas23@gmail.com>
2022-05-03 20:02:44 +0530

    scripts: update scripts
    Signed-off-by: TejasKamat <kamattejas23@gmail.com>

--- clone.sh
+++ clone.sh
@@ -1,41 +1,102 @@
 #!/bin/sh
-# export PE=1
-# export OCTAVI=1
 
-if [ "$OCTAVI" = "1" ]
+echo ""
+read -p  "Enter rom name (1=Octavi-Os, 2=Project-Zephyrus, 3=Evolution-X): " RM
+
+i=1
+fn=$PWD/device/xiaomi/mojito
+while [ "$i" -lt 2 ]
+do
+	if [ -f $fn ]
+	then
+		break
+	fi
+	
+	case $RM in
+		1 )
+			echo ""
+			echo "Cloning dependencies for Octavi-Os"
+			sleep 1
+			git clone https://github.com/Tejaskamat/device_xiaomi_mojito.git -b 12 device/xiaomi/mojito ;;
+		2 )
+			echo ""
+			echo -n "Cloning dependencies for Project-Zephyrus"
+			sleep 1
+			git clone https://github.com/TejasKamat/device_xiaomi_mojito.git -b zephyrus device/xiaomi/mojito ;;
+		3 )
+			echo ""
+			echo "Cloning dependencies for Evolution-X"
+			sleep 1
+			git clone https://github.com/TejasKamat/device_xiaomi_mojito.git -b snow device/xiaomi/mojito ;;
+		* )
+			echo "Not Defined :("
+		        exit 1 ;;
+esac
+
+(( i++ ))
+done
+echo ""
+function clone_depth1 () {
+	git clone https://github.com/TejasKamat/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+	echo ""
+	git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+	echo ""
+	git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_mojito.git vendor/xiaomi/mojito
+}
+function clone () {
+        git clone https://github.com/TejasKamat/device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+        echo ""
+	git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+        echo ""
+	git clone https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_mojito.git vendor/xiaomi/mojito
+}
+
+hx=$PWD/hardware/xiaomi
+if [ -f $hx ]
 then
-	echo "cloning dependencies for OctaviOs"
-
-	git clone https://github.com/DrTK001/device_xiaomi_mojito.git -b 12 device/xiaomi/mojito
-
-	git clone https://github.com/DrTK001/device_xiaomi_sm6150-common.git -b 12 device/xiaomi/sm6150-common
-else
-	echo "cloning dependencies for pixel blaster"
-
-	git clone https://github.com/DrTK001/device_xiaomi_mojito.git -b 12-pixel-blaster device/xiaomi/mojito
-
-	git clone https://github.com/DrTK001/device_xiaomi_sm6150-common.git -b 12-wip2 device/xiaomi/sm6150-common
+         echo " $hx exists . . . removing..."
+         echo ""
+         sleep 2
+         rm -rf $PWD/hardware/xiaomi
+         git clone https://github.com/TejasKamat/android_hardware_xiaomi.git -b arrow-12.0  hardware/xiaomi
 fi
 
-git clone https://github.com/TejasKamat/device_xiaomi_sm6150-extras.git device/xiaomi/sm6150-extras
+sleep 2
+read -p "Would you like to do a shallow clone ? (Y/n)" depth
+echo ""
 
-if [ "$PE" = "1" ]
+if [ "$depth" -eq "Y" ]
 then
-    echo "cloning vendor from PE"
-
-    git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
-
-    git clone --depth=1 https://gitlab.pixelexperience.org/android/vendor-blobs/vendor_xiaomi_mojito.git vendor/xiaomi/mojito
+	clone_depth1
 else
-        echo "cloning vendor from xiaomi-sdm678"
-
-        git clone --depth=1 https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_mojito.git -b 12 vendor/xiaomi/mojito
-
-        git clone --depth=1 https://gitlab.com/xiaomi-sdm678/android_vendor_xiaomi_sm6150-common.git -b 12 vendor/xiaomi/sm6150-common
+	clone
 fi
 
-git clone https://github.com/xiaomi-sdm678/android_kernel_xiaomi_mojito.git --depth=1 kernel/xiaomi/sm6150
+read -p "Enter kernel name (1=WestCoast, 2=legionX 3=NetErnels: " kernel
+while [ "$i" -lt 2 ]
+do
+	case $kernel in 
+		1 )
+			echo ""
+			echo "cloning WestCoast kernel . . ."
+			echo ""
+			sleep 1
+			git clone https://github.com/xiaomi-sdm678/android_kernel_xiaomi_mojito.git --depth=1 kernel/xiaomi/sm6150 ;;
+		2 )
+			echo ""
+			echo "Cloning legionX kernel . . ."
+			echo ""
+			sleep 1
+			git clone https://github.com/venom-stark/kernel_xiaomi_mojito.git --depth=1 kernel/xiaomi/mojito ;;
+		3 )
+			echo ""
+			echo "Cloning NetErnels kernel . . ."
+			echo ""
+			sleep 1
+			git clone https://github.com/Neternels/android_kernel_xiaomi_mojito.git --depth=1 kernel/xiaomi/mojito ;;
+		* )
+			echo "Invalid option :( "
+			exit 1 ;;
+	esac
+done
 
-git clone https://github.com/kdrag0n/proton-clang --depth=1 prebuilts/clang/host/linux-x86/clang-proton
-
-git clone https://github.com/DrTK001/android_hardware_xiaomi.git -b arrow-12.0  hardware/xiaomi
